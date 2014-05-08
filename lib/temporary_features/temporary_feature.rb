62aa6f5c1b359b8fa34587c490013a4a3399aa8f
Abelardo <abelardogilm@gmail.com>
2014-05-08 17:50:24 +0200

    Set default temporary values

--- lib/temporary_features/temporary_feature.rb
+++ lib/temporary_features/temporary_feature.rb
@@ -2,12 +2,14 @@ module TemporaryFeatures
   class TemporaryFeature
     def initialize(id)
       settings = settings_for(id)
+      settings ||= {}
       @start_time = settings["from"]
-      @end_time   = settings["to"]
+      @end_time   = settings["to"] || "2099-12-31 23:59:59 +01:00"
     end
 
     def enabled?
       now = Time.now
+      return false unless @start_time
       (@start_time <= now) and (now <= @end_time)
     end
 
